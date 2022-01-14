c4bdc9efde1525328556898150732edadfb4e7a9
priyanshi-mittal <80943199+priyanshi-mittal@users.noreply.github.com>
2022-01-14 21:53:59 +0530

    Demo prog

---
+++ Demo.java
@@ -0,0 +1,13 @@
+public class Demo 
+{
+    void func()
+    {
+        System.out.println("DS Started....");
+    }
+    public static void main(String args[])
+    {
+        Demo obj= new Demo();
+        obj.func();
+    }
+    
+}
